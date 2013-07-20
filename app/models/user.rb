e0954604924cd0daca221c3ff13e4be820b42042
Miłosz Osiński <mylosz@yahoo.pl>
2013-07-20 15:18:02 +0200

    poprawienie walidacji usera

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
     acts_as_authentic
     
     has_private_messages
+
+  validates :pesel, length: { in: 11..11 }, allow_blank: false
+  validates :name_surname, :format => { :with => /^[^0-9`!@#\$%\^&*+_=]+$/, :message => 'ma niepoprawny format.' }, allow_blank: false
 end
