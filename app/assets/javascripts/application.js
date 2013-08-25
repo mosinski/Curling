0079fcb2caa40d05ab5bc7b6d89b807e221eb4d8
Miłosz Osiński <mylosz@yahoo.pl>
2013-08-25 23:32:34 +0200

    czyszczenie przed popover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,7 +58,8 @@ jQuery(document).ready(function() {
 $('a[rel=popover]').popover({
   html: true,
   trigger: 'hover',
-  placement: 'right'
+  placement: 'right',
+  content: function(){return '<img src="'+$(this).data('img') + '" />';}
 });
 
 
