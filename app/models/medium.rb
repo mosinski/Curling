7f6b0fe95df989bf9e8d918c9d54abc4b92373be
Miłosz Osiński <mylosz@yahoo.pl>
2013-09-08 23:57:00 +0200

    poprawienie validacji linku w media o nas

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,3 +1,6 @@
 class Medium < ActiveRecord::Base
   attr_accessible :tytul, :url
+  
+    validates :tytul, length: { maximum: 100 }, allow_blank: false
+    validates :url, length: { maximum: 150 }, allow_blank: false
 end
