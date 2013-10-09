47c9fb7288b3766b29aaa93fae1fdc4d4dcf78d3
Miłosz Osiński <mylosz@yahoo.pl>
2013-10-09 12:49:17 +0200

    dodanie nowych zdjęc i poprawki przygotowanie do assets pipline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -90,3 +90,16 @@ $(document).ready(function() {
     $("div#"+parentId ).slideUp();
   });
 });
+
+jQuery(document).ready(function() {
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  6000);
+});
