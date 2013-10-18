12e23d4049f81c2eda1fcf04f631e08db356bdb7
Miłosz Osiński <mylosz@yahoo.pl>
2013-10-18 17:20:41 +0200

    poprawienie wyswietlanych artykułów z 30 na 10

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,7 +3,7 @@ class NewsController < ApplicationController
   # GET /news
   # GET /news.json
   def index
-    @news = News.paginate(:page => params[:page], :per_page => 30, :order => 'created_at DESC').search(params[:search], params[:page])
+    @news = News.paginate(:page => params[:page], :per_page => 10, :order => 'created_at DESC').search(params[:search], params[:page])
     @albumy_z_news = Album.find_all_by_przydzial("news")
     
     respond_to do |format|
