f08151a6b5e16ec77e4a8740b96da9f5efe2dde4
Miłosz Osiński <mylosz@yahoo.pl>
2013-12-12 23:52:33 +0100

    poprawienie wyszukiwania

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,21 +9,22 @@ class ApplicationController < ActionController::Base
 
     I18n.locale = session[:locale] || I18n.default_locale
   end
-  
+
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
-  { locale: I18n.locale }
+    { locale: I18n.locale }
   end
 
   helper_method :current_user
-  private
-    def current_user_session
-	return @current_user_session if defined?(@current_user_session)
-	@current_user_session = UserSession.find
-    end
 
-    def current_user
-	return @current_user if defined?(@current_user)
-	@current_user = current_user_session && current_user_session.record
-    end
+  private
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -7,6 +7,6 @@ class News < ActiveRecord::Base
   
   def self.search(search, page)
   paginate :per_page => 10, :page => page,
-           :conditions => ['tytul like :q or tekst_pl like :q or tekst_en like :q', q: "%#{search}%"], :order => 'termin'
+           :conditions => ['tytul ILIKE :q or tekst_pl ILIKE :q or tekst_en ILIKE :q', q: "%#{search}%"], :order => 'termin'
   end
 end
