587ae4f6e3689c84d80212cdb6b919fb639d3d6a
Miłosz Osiński <mylosz@yahoo.pl>
2014-02-16 12:59:53 +0100

    poprawienie locales i dodanie kilku tłumaczeń

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,6 @@ class ApplicationController < ActionController::Base
     I18n.locale = session[:locale] || I18n.default_locale
   end
 
-  def default_url_options(options={})
-    logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { locale: I18n.locale }
-  end
-
   helper_method :current_user
 
   private
